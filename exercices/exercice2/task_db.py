549ab022a61bece74669b51ab8c626f94be0a657
vaiyd <baptiste.meg@gmail.com>
2023-10-19 08:21:20 +0200

    Add : Exercice 2 et 3

---
+++ exercices/exercice2/task_db.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+'''
+
+Exercice : Gestionnaire de tâches avec une base de données
+ 
+Installez SQLAlchemy si vous ne l'avez pas déjà fait.
+Créez un fichier SQLite tasks.db.
+Définissez une classe Task avec SQLAlchemy qui représente les éléments de votre liste de tâches. Chaque tâche doit avoir les attributs suivants :
+    ID (clé primaire)
+    Title (titre de la tâche)
+    Completed (indique si la tâche a été complétée ou non, booléen)
+Écrivez des procdéures ou fonctions pour effectuer les opérations CRUD suivantes :
+    Créer une nouvelle tâche
+    Récupérer toutes les tâches
+    Mettre à jour le statut d'une tâche (complétée ou non complétée)
+    Supprimer une tâche
+
+'''
+
+def main():
+    print("hello")
+
+if __name__ == '__main__':
+    main()
+
---
+++ exercices/exercice3/task_api.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+'''
+
+Exercice 3 : Gestionnaire de tâches avec une base de données et une API permettant l'accès
+
+1. Installez Flask et Flask-RESTful si vous ne l'avez pas déjà fait.
+
+2. Créez un nouveau fichier pour votre application Flask.
+
+3. Configurez une API avec Flask-RESTful et créez des routes qui correspondent aux opérations CRUD que nous
+avons définies précédemment (par exemple, GET pour récupérer les tâches, POST pour créer une nouvelle tâche,
+PUT pour mettre à jour une tâche, et DELETE pour supprimer une tâche).
+
+4. Chaque route doit interagir avec la base de données, effectuant l'opération
+correspondante en fonction de la requête reçue.
+
+5. Verfier votre API en utilisant un outil comme curl ou Postman pour 
+envoyer des requêtes à votre application et observer les réponses.
+
+'''
+
+def main():
+    print("hello")
+
+if __name__ == '__main__':
+    main()
